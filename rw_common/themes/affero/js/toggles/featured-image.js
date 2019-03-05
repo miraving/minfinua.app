27e7ed5d05305dc1633733d47a7fa937e803129a
Vitalii Obertynskyi <vitaliy.obertinskiy@icloud.com>
2019-03-05 19:29:24 +0200

    init commit

--- rw_common/themes/affero/js/toggles/featured-image.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-	if ($('.affero-content').find('img:first').length > 0 && jQuery(".album-title").length == 0) {
-		$(".affero-banner").html($('.affero-content').find('img:first'));
-	}
-});
